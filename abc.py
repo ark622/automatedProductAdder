e6b9a591b0c2ef912596f37f4943744e209b8386
ABDULREHMAN <rehmankhld@gmail.com>
2019-09-30 00:26:50 +0500

    done

--- abc.py
+++ abc.py
@@ -23,9 +23,15 @@ with open("Ouput_Username.csv", errors='ignore') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=",")
     productType = "simple"
     accData = list(readCSV)
-    startFrom = 43   # Start from which row
+    startFrom = 3   # Start from which row
     i = startFrom
-    for row in accData[startFrom:]:
+    noOfSkips = 0
+    productAdded = 0
+    for row in accData[startFrom:16]:
+        if(noOfSkips!=0):
+            noOfSkips -= 1
+            i += 1
+            continue
         productType = "simple"
         if(accData[i+1][2] == row[2]):
             productType = "variation"
@@ -81,13 +87,10 @@ with open("Ouput_Username.csv", errors='ignore') as csvfile:
                 k += 2
             k=3
             while(row[k]):
-                #response.find_elements_by_xpath("//button[@class='button add_attribute']")[0].click()
-                #time.sleep(3)
                 success = 0
                 while(success == 0):
                     try:
                         dropDownName = response.find_elements_by_xpath("//input[@name='attribute_names[{}]']".format(attrAdded))
-                        print(row[k])
                         dropDownName[0].send_keys(row[k])
                         success = 1
                     except:
@@ -96,10 +99,12 @@ with open("Ouput_Username.csv", errors='ignore') as csvfile:
                         response.implicitly_wait(20)
                         success = 0
                 options = ""
+                totalrows = 0
                 l = i
                 while(accData[l][2] == row[2]):
                     options = options + "|" + accData[l][k+1]
                     l += 1
+                    totalrows += 1
                 success = 0
                 while(success == 0):
                     try:
@@ -174,10 +179,116 @@ with open("Ouput_Username.csv", errors='ignore') as csvfile:
                     success = 1
                 except:
                     response.implicitly_wait(0)
-                    weWait = WebDriverWait(response, 10).until(EC.alert_is_present())
+                    weWait = WebDriverWait(response, 60).until(EC.alert_is_present())
                     response.implicitly_wait(20)
                     success = 0
             alert.accept()
-        break                                    
-    i+=1
-    #break
+            expands = response.find_elements_by_xpath("//strong[contains(text(),'#')]")
+            success = 0
+            while(success == 0):
+                try:
+                    expands[0].click()
+                    success = 1
+                except:
+                    success = 0
+            for xpnd in expands[1:]:
+                time.sleep(0.5)
+                xpnd.click()
+            line = 1
+            count1 = 0
+            for x in range(totalrows):
+                if(count1 == 15):
+                    count1 = 0
+                if(line == 16):
+                    line = 1
+                    success = 0
+                    while(success == 0):
+                        try:
+                            response.find_element_by_xpath("//div[@id='variable_product_options_inner']//div[@class='toolbar']").click()
+                            response.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[4]/button[1]").click()
+                            success = 1
+                        except:
+                            response.implicitly_wait(0)
+                            weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[4]/button[1]")))
+                            response.implicitly_wait(20)
+                            success = 0
+                    response.implicitly_wait(0)
+                    weWait = WebDriverWait(response, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='button-primary save-variation-changes' and @disabled]")))
+                    response.implicitly_wait(20)
+                    success = 0
+                    time.sleep(3)
+                    while(success == 0):
+                        try:
+                            response.find_element_by_xpath("//div[@class='toolbar']//a[@class='next-page']").click()
+                            success = 1
+                        except:
+                            response.implicitly_wait(0)
+                            weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='toolbar']//a[@class='next-page']")))
+                            response.implicitly_wait(20)
+                            success = 0
+                    success = 0
+                    while(success == 0):
+                        try:
+                            response.find_element_by_xpath("//div[@id='variable_product_options_inner']//div[@class='toolbar']").click()
+                            success = 1
+                        except:
+                            response.implicitly_wait(0)
+                            weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='variable_product_options_inner']//div[@class='toolbar']")))
+                            response.implicitly_wait(20)
+                            success = 0
+                    time.sleep(4)
+                    response.execute_script("window.scrollTo(0, 0)") 
+                    expands = response.find_elements_by_xpath("//strong[contains(text(),'#')]")
+                    success = 0
+                    while(success == 0):
+                        try:
+                            expands[0].click()
+                            success = 1
+                        except:
+                            success = 0
+                    for xpnd in expands[1:]:
+                        time.sleep(0.5)
+                        xpnd.click()
+                success = 0
+                while(success == 0):
+                    try:
+                        regPrice = response.find_element_by_xpath("//input[@id='variable_regular_price_{}']".format(count1))
+                        regPrice.send_keys(accData[i+x][15])
+                        success = 1
+                    except:
+                        response.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[3]/div[{}]/h3[1]/strong[1]".format(line+1)).click()
+                        response.implicitly_wait(0)
+                        weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='variable_regular_price_{}']".format(count1))))
+                        response.implicitly_wait(20)
+                        success = 0
+                line += 1
+                count1 += 1
+            success = 0
+            while(success == 0):
+                try:
+                    response.find_element_by_xpath("//div[@id='variable_product_options_inner']//div[@class='toolbar']").click()
+                    response.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[4]/button[1]").click()
+                    success = 1
+                except:
+                    response.implicitly_wait(0)
+                    weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[7]/div[1]/div[4]/button[1]")))
+                    response.implicitly_wait(20)
+                    success = 0
+            response.implicitly_wait(0)
+            weWait = WebDriverWait(response, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='button-primary save-variation-changes' and @disabled]")))
+            response.implicitly_wait(20)
+            noOfSkips = totalrows - 1
+        success = 0
+        response.execute_script("window.scrollTo(0, 0)") 
+        while(success == 0):
+            try:
+                response.find_element_by_xpath("/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/input[2]").click()
+                success = 1
+            except:
+                response.implicitly_wait(0)
+                weWait = WebDriverWait(response, 10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[2]/div[2]/div[1]/div[5]/form[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/input[2]")))
+                response.implicitly_wait(20)
+                success = 0    
+        productAdded += 1
+        print("product(s) added: "+ str(productAdded))
+        i+=1
